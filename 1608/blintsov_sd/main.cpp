95228f3a652f3b5c3296cf0a219fb391ccd35ce3
Sergey Blintsov <sblin180@gmail.com>
2018-10-31 18:35:49 +0300

    Algorithm released. MPI connected to project

--- 1608/blintsov_sd/main.cpp
+++ 1608/blintsov_sd/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "mpi.h"
+
+double current_function(double x)
+{
+    return 2*x-x;
+}
+
+double find_integral(double a, double b, int steps) {
+    double result = .0;
+
+    double step = (b-a)/steps;
+    for (double x = a; x <= b; x+=step)
+        result += current_function(x-step/2);
+    result *= step;
+    return result;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << find_integral(1, 10, 20);
 }
