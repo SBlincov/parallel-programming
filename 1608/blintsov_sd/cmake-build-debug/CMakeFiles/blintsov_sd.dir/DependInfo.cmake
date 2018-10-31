d03f1e85c9f481f052c556a342f9009f263edb71
Sergey Blintsov <sblin180@gmail.com>
2018-10-31 17:22:59 +0300

    Project created

---
+++ 1608/blintsov_sd/cmake-build-debug/CMakeFiles/blintsov_sd.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sblincov/Learning/programming/parallel/ex1/parallel-programming/1608/blintsov_sd/main.cpp" "/Users/sblincov/Learning/programming/parallel/ex1/parallel-programming/1608/blintsov_sd/cmake-build-debug/CMakeFiles/blintsov_sd.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 1608/blintsov_sd/cmake-build-debug/CMakeFiles/blintsov_sd.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for blintsov_sd.
+# This may be replaced when dependencies are built.
